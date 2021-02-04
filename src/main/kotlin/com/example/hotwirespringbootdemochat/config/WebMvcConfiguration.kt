31f09ef091bf39afbc549baaf757db8d89967373
lazaronixon <lazaronixon@hotmail.com>
2021-02-04 20:13:38 -0300

    cosmetic

--- src/main/kotlin/com/example/hotwirespringbootdemochat/config/WebMvcConfiguration.kt
+++ src/main/kotlin/com/example/hotwirespringbootdemochat/config/WebMvcConfiguration.kt
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class webMvcConfiguration : WebMvcConfigurer {
+class WebMvcConfiguration : WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.addViewController("/").setViewName("forward:rooms")
     }
