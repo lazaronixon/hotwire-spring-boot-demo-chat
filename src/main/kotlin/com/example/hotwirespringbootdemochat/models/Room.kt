f8f8b1fc66877cbdf9a4d8d42b67eb0dfa7df2e8
lazaronixon <lazaronixon@hotmail.com>
2021-02-04 14:46:35 -0300

    add liquibase

--- src/main/kotlin/com/example/hotwirespringbootdemochat/models/Room.kt
+++ src/main/kotlin/com/example/hotwirespringbootdemochat/models/Room.kt
@@ -1,17 +1,16 @@
 package com.example.hotwirespringbootdemochat.models
 
 import java.time.LocalDateTime
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
+import javax.persistence.GenerationType.*
+
 
 @Entity @Table(name = "rooms")
 class Room(
     var name: String? = null,
     override var createdAt: LocalDateTime? = null,
     override var updatedAt: LocalDateTime? = null,
-    @Id @GeneratedValue override var id: Long? = null
+    @Id @GeneratedValue(strategy = IDENTITY) override var id: Long? = null
 ) : ApplicationRecord<Long?>() {
 
     fun merge(other: Room) : Room {
